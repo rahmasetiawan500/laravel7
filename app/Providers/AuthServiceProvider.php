61d52881193ec71416c3a3b605eeee18dbb01bf9
rahmasetiawan500 <rahmasetiawan500@gmail.com>
2020-07-14 18:27:48 +0700

    role

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,5 +26,8 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+        Gate::before(function ($user){
+            return$user->isAdmin() ? true : null;
+        });
     }
 }
